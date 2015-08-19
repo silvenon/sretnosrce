3f2fcb856e61ed0f710024ed80fe4301e6b95704
Matija Marohnić <matija.marohnic@gmail.com>
2015-08-19 20:33:52 +0200

    Improve form and remove subject field

--- scripts/form.js
+++ scripts/form.js
@@ -6,19 +6,37 @@
   if (form) {
     var email = form.querySelector('[type="email"]');
     var checkbox = form.querySelector('[type="checkbox"]');
-    var description = form.querySelector('.form-checkbox .form-description');
 
-    checkbox.setAttribute('disabled', 'disabled');
+    var hidden = form.querySelectorAll('.email-hidden');
+    var visible = form.querySelectorAll('.email-visible');
 
-    email.addEventListener('keyup', function () {
-      if (email.value) {
+    console.log(hidden, visible);
+
+    function updateState(emailExists) {
+      if (emailExists) {
         checkbox.removeAttribute('disabled');
-        description.style.display = 'none';
+        Array.prototype.forEach.call(visible, function (el) {
+          el.style.display = '';
+        });
+        Array.prototype.forEach.call(hidden, function (el) {
+          el.style.display = 'none';
+        });
       } else {
         checkbox.checked = true;
         checkbox.setAttribute('disabled', 'disabled');
-        description.style.display = '';
+        Array.prototype.forEach.call(visible, function (el) {
+          el.style.display = 'none';
+        });
+        Array.prototype.forEach.call(hidden, function (el) {
+          el.style.display = '';
+        });
       }
+    }
+
+    updateState(false);
+
+    email.addEventListener('keyup', function () {
+      updateState(!!email.value);
     });
   }
 
