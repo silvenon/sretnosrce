7006c36364fec8f371ef679c95df72fbd94ef6a1
Matija Marohnić <matija.marohnic@gmail.com>
2015-08-20 13:37:59 +0200

    Conditionally load scripts

--- pitaj/index.md
+++ pitaj/index.md
@@ -2,6 +2,7 @@
 layout: site
 title: Pitaj me nešto
 excerpt: Kako bih te mogao lakše osloviti, možeš si izmisliti pseudonim, kao "zbunjen", "znatiželjna", "maslačak" itd.
+script: form
 ---
 
 # Pitaj me nešto
--- scripts/form.js
+++ scripts/form.js
@@ -1,32 +1,29 @@
 (function() {
 
   var form = document.querySelector('.form');
+  var email = form.querySelector('[type="email"]');
+  var checkbox = form.querySelector('[type="checkbox"]');
+  var hidden = form.querySelectorAll('.email-hidden');
+  var visible = form.querySelectorAll('.email-visible');
+  var forEach = Array.prototype.forEach.call;
 
-  if (form) {
-    var email = form.querySelector('[type="email"]');
-    var checkbox = form.querySelector('[type="checkbox"]');
-    var hidden = form.querySelectorAll('.email-hidden');
-    var visible = form.querySelectorAll('.email-visible');
-    var forEach = Array.prototype.forEach.call;
-
-    function updateState(emailExists) {
-      if (emailExists) {
-        checkbox.removeAttribute('disabled');
-        forEach(visible, function (el) { el.style.display = ''; });
-        forEach(hidden,  function (el) { el.style.display = 'none'; });
-      } else {
-        checkbox.checked = true;
-        checkbox.setAttribute('disabled', 'disabled');
-        forEach(visible, function (el) { el.style.display = 'none'; });
-        forEach(hidden,  function (el) { el.style.display = ''; });
-      }
+  function updateState(emailExists) {
+    if (emailExists) {
+      checkbox.removeAttribute('disabled');
+      forEach(visible, function (el) { el.style.display = ''; });
+      forEach(hidden,  function (el) { el.style.display = 'none'; });
+    } else {
+      checkbox.checked = true;
+      checkbox.setAttribute('disabled', 'disabled');
+      forEach(visible, function (el) { el.style.display = 'none'; });
+      forEach(hidden,  function (el) { el.style.display = ''; });
     }
-
-    updateState(false);
-
-    email.addEventListener('keyup', function () {
-      updateState(!!email.value);
-    });
   }
+
+  updateState(false);
+
+  email.addEventListener('keyup', function () {
+    updateState(!!email.value);
+  });
 
 }());
