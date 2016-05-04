994ce93ae1c249eecf80d7a9b17c296ebb9b8b17
Matija Marohnić <matija.marohnic@gmail.com>
2016-05-04 10:22:04 +0200

    Remove tests, it's an overkill

--- test/helpers/common.js
+++
@@ -1,7 +0,0 @@
-import chai from 'chai';
-import chaiAsPromised from 'chai-as-promised';
-
-chai.should();
-chai.use(chaiAsPromised);
-
-chaiAsPromised.transferPromiseness = browser.transferPromiseness;
